2c20bfc9359718b6e5a753287d05fce74df4df82
Fauzan <laode.fauzan@accenture.com>
2022-08-21 18:14:07 +0700

    fix flyway script

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,41 +1,39 @@
-CREATE TABLE customer (
-  id varchar(100) NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
-  PRIMARY KEY (id)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS customer (
+    id varchar(100) NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
 
 CREATE TABLE wallet (
-  id varchar(100) NOT NULL AUTO_INCREMENT,
+  id varchar(100) NOT NULL,
   customer_id varchar(100) NOT NULL,
   status boolean  NOT NULL,
-  enabled_at TIMESTAMP NOT NULL
-  disabled_at TIMESTAMP NOT NULL
+  enabled_at TIMESTAMP NOT NULL,
+  disabled_at TIMESTAMP NOT NULL,
   balance DECIMAL(20,2) NOT NULL,
   PRIMARY KEY (id),
-  foreign key (customer) references customer(id)
+  foreign key (customer_id) references customer(id)
 );
 
 CREATE TABLE deposit (
-  id varchar(100) NOT NULL AUTO_INCREMENT,
+  id varchar(100) NOT NULL ,
   customer_id varchar(100) NOT NULL,
   wallet_id varchar(100) NOT NULL,
-  deposited_at TIMESTAMP NOT NULL
+  deposited_at TIMESTAMP NOT NULL,
   amount DECIMAL(20,2) NOT NULL,
   reference_id varchar(100) NOT NULL,
   PRIMARY KEY (id),
-  foreign key (customer) references customer(id),
-  foreign key (wallet) references wallet(id)
+  foreign key (customer_id) references customer(id),
+  foreign key (wallet_id) references wallet(id)
 );
 
 CREATE TABLE withdraw (
-  id varchar(100) NOT NULL AUTO_INCREMENT,
+  id varchar(100) NOT NULL ,
   customer_id varchar(100) NOT NULL,
   wallet_id varchar(100) NOT NULL,
-  withdrawn_at TIMESTAMP NOT NULL
+  withdrawn_at TIMESTAMP NOT NULL,
   amount DECIMAL(20,2) NOT NULL,
   reference_id varchar(100) NOT NULL,
   PRIMARY KEY (id),
-  foreign key (customer) references customer(id),
-  foreign key (wallet) references wallet(id)
+  foreign key (customer_id) references customer(id),
+  foreign key (wallet_id) references wallet(id)
 );
-
